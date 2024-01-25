dc4f69b64bb18ac49af02825196b72a126f6418b
Daniel Ofodile <ofodiledaniel20@gmail.com>
2024-01-25 21:09:50 +0100

    create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase.
+ *
+ * Return: void.
+ */
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
