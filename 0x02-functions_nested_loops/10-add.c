41b5c232b27cd11ca992394195beab4a2fcc5d3e
Daniel Ofodile <ofodiledaniel20@gmail.com>
2024-01-26 00:07:37 +0100

    creates 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers.
+ *
+ * @a: parameter to add.
+ * @b: parameter to add.
+ * Return: sum.
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
