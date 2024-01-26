eb7dd2fbaf74aa62aff24dab6cca7ff99c1605a8
Daniel Ofodile <ofodiledaniel20@gmail.com>
2024-01-27 00:30:44 +0100

    create 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ *
+ * Return: void.
+ */
+void times_table(void)
+{
+	int rows = 10;
+	int columns = 10;
+
+	int row = 0;
+
+	while (row < rows)
+	{
+		int column = 0;
+
+		while (column < columns)
+		{
+			int product = row * column;
+			
+			if (product < 10)
+			{
+				_putchar(product + '0');
+			}
+				else
+				{
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+				}
+			if (column < columns -1)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			column++;
+		}
+		_putchar('\n');
+		row++;
+	}
+}
