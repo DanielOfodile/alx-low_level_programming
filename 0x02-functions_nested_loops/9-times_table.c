895a478dd76d56598b2bc28d8a8f9ed5ea506809
Daniel Ofodile <ofodiledaniel20@gmail.com>
2024-01-27 01:15:23 +0100

    fix betty and output error

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,9 +19,10 @@ void times_table(void)
 		while (column < columns)
 		{
 			int product = row * column;
-			
+
 			if (product < 10)
 			{
+				_putchar(' ');
 				_putchar(product + '0');
 			}
 				else
@@ -29,7 +30,7 @@ void times_table(void)
 					_putchar((product / 10) + '0');
 					_putchar((product % 10) + '0');
 				}
-			if (column < columns -1)
+			if (column < columns - 1)
 			{
 				_putchar(',');
 				_putchar(' ');
