2664573a4b966add0d9999816a82c014cfdf83fe
Daniel Ofodile <ofodiledaniel20@gmail.com>
2024-01-29 17:34:19 +0100

    creates 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0.
+ *
+ * @n: parameter to print.
+ * Return: void.
+ */
+void print_times_table(int n)
+{
+	int rows = n;
+	int columns = n;
+
+	int row = 0;
+
+	if (n < 0 || n > 15)
+		return;
+	while (row < rows)
+	{
+		int column = 0;
+
+		while (column < columns)
+		{
+			int product = row * column;
+
+			if (column == 0)
+			{
+				_putchar(product + '0');
+			}
+				else if (product < 10)
+				{
+					_putchar(' ');
+					_putchar(product + '0');
+				}
+				else
+				{
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+				}
+			if (column < columns - 1)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			column++;
+		}
+		_putchar('\n');
+		row++;
+	}
+}
