05a4679eae1eb21057717bed97af7fcb48d45b9e
Daniel Ofodile <ofodiledaniel20@gmail.com>
2024-01-29 18:15:54 +0100

    fix output error

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,17 +30,27 @@ void print_times_table(int n)
 				else if (product < 10)
 				{
 					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(product + '0');
+				}
+				else if (product < 100)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
 				}
 				else
 				{
-					_putchar((product / 10) + '0');
+					_putchar(' ');
+					_putchar((product / 100) + '0');
+					_putchar(((product % 100) / 10) + '0');
 					_putchar((product % 10) + '0');
 				}
 			if (column < columns - 1)
 			{
 				_putchar(',');
-				_putchar(' ');
 			}
 
 			column++;
