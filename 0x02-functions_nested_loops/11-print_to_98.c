0ca95706ac0aef9672448dd4f65df15b81f5afb3
Daniel Ofodile <ofodiledaniel20@gmail.com>
2024-01-30 19:26:16 +0100

    use standard library

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,59 +1,22 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_to_98 - prints all natural numbers from n to 98.
- *
- * @n: parameter to print.
+ * print_to_98 - print to 98.
+ * @n: number to start from.
+ * Return: void.
  */
 void print_to_98(int n)
 {
-	if (n < 98)
+	while (n < 98)
 	{
-		while (n <= 98)
-		{
-			if (n < 0)
-			{
-				_putchar('-');
-				_putchar(-n + '0');
-			}
-			else if (n < 10)
-				_putchar(n + '0');
-			else
-			{
-				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
-			}
-			if (n != 98)
-				_putchar(',');
-			_putchar(' ');
-			n++;
-		}
+		printf("%i, ", n);
+		n++;
 	}
-	else if (n > 98)
+	while (n > 98)
 	{
-		while (n >= 98)
-		{
-			if (n < 100)
-			{
-				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
-			}
-			else
-			{
-				_putchar(n / 100 + '0');
-				_putchar((n % 100) / 10 + '0');
-				_putchar(n % 10 + '0');
-			}
-			if (n != 98)
-				_putchar(',');
-			_putchar(' ');
-			n--;
-		}
+		printf("%i, ", n);
+		n--;
 	}
-	else
-	{
-		_putchar('9');
-		_putchar('8');
-	}
-	_putchar('\n');
+	printf("98");
+	printf("\n");
 }
