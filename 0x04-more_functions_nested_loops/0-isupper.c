c9622b4f78092b61c43727dbeac9e8ddb8c08207
Daniel Ofodile <ofodiledaniel20@gmail.com>
2024-02-02 17:22:56 +0100

    creates 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character.
+ * @c: character to check.
+ * Return: 1 or 0.
+ */
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+		return(1);
+	else
+		return(0);
+}
